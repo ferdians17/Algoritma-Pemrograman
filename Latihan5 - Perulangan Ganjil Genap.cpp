4261767d7482e0d92cb4f2b06fcdceb25c27c1c9
ferdians17 <penyalahgunaanakun@gmail.com>
2024-09-09 20:22:05 +0700

    Update Latihan5 - Perulangan Ganjil Genap.cpp
    Ganjil dan Genap menggunakan Perulangan

--- Latihan5 - Perulangan Ganjil Genap.cpp
+++ Latihan5 - Perulangan Ganjil Genap.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main()
 {
+	cout << "NILAI I \t Keterangan \n";
+	cout << "=======================\n";
 	
 	for(int angka = 1; angka <= 10; angka++)
 	{
 		if( angka %2 == 0) cout << angka << ". \t Genap \n";
 		else cout << angka << ". Ganjil \n";
 	}
+	
+	cout << "=======================\n";
 	
 	return 0;
 }
